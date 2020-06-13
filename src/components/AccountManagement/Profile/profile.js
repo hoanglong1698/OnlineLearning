87f2e26400e0ce8d4c01b4d148d2112b2b0d28d5
Hoang Long <1612352@student.hcmus.edu.vn>
2020-06-13 18:08:08 +0700

    Sửa giao diện profile

--- src/components/AccountManagement/Profile/profile.js
+++ src/components/AccountManagement/Profile/profile.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import { View, Text, TouchableOpacity, StyleSheet, Image, Alert } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Image, TextInput } from 'react-native'
 import { ListItem } from 'react-native-elements'
 import { color } from './../../../globals/constants';
-import InputView from '../../Common/input-view'
-import PasswordView from '../../Common/password-view'
-import LogoutButton from '../../Common/log-out-button';
 
-const Profile = ({navigation}) => {
+const Profile = ({ navigation }) => {
     const list = [
         {
             title: 'Change password',
@@ -28,9 +25,6 @@ const Profile = ({navigation}) => {
         },
     ]
 
-    const onPressTest = () =>
-        console.log('clicked')
-
     return (
         <View style={styles.container}>
             <View style={styles.userInfo}>
@@ -41,16 +35,42 @@ const Profile = ({navigation}) => {
                     <Text style={styles.name}>Hoang Long Nguyen</Text>
                 </TouchableOpacity>
 
-                <Text style={styles.nameOfInput}>Your Name</Text>
-                <InputView title="Hoang Long Nguyen"></InputView>
+                <Text style={styles.labelOfInput}>Your Name</Text>
+                <View style={styles.inputView} >
+                    <TextInput
+                        style={styles.inputText}
+                        placeholder='Username'
+                        placeholderTextColor={color.placeholderTextColor}
+                        defaultValue='Hoang Long Nguyen'
+                    //onChangeText={text => setUsername(text)}
+                    />
+                </View>
 
-                <Text style={styles.nameOfInput}>Username</Text>
-                <InputView title="hoanglong1698"></InputView>
+                <Text style={styles.labelOfInput}>Username</Text>
+                <View style={styles.inputView} >
+                    <TextInput
+                        style={styles.inputText}
+                        placeholder='Username'
+                        placeholderTextColor={color.placeholderTextColor}
+                        defaultValue='hoanglong1698'
+                    //onChangeText={text => setUsername(text)}
+                    />
+                </View>
 
-                <Text style={styles.nameOfInput}>Email</Text>
-                <InputView title="hoanglong16198@gmail.com"></InputView>
+                <Text style={styles.labelOfInput}>Email</Text>
+                <View style={styles.inputView} >
+                    <TextInput
+                        style={styles.inputText}
+                        placeholder='Username'
+                        placeholderTextColor={color.placeholderTextColor}
+                        defaultValue='hoanglong16198@gmail.com'
+                    //onChangeText={text => setUsername(text)}
+                    />
+                </View>
 
-                <LogoutButton title="Log out"></LogoutButton>
+                <TouchableOpacity style={styles.changeInfoButton}>
+                    <Text style={styles.changeInfoText}>Change Info</Text>
+                </TouchableOpacity>
             </View>
 
             <View style={{ marginTop: 30 }}>
@@ -75,6 +95,17 @@ const Profile = ({navigation}) => {
                         fontWeight: 'bold',
                     }}
                     onPress={() => navigation.navigate('Setting')}
+                />
+                <ListItem
+                    key={3}
+                    title='Logout'
+                    bottomDivider
+                    chevron
+                    titleStyle={{
+                        color: 'red',
+                        fontWeight: 'bold',
+                    }}
+                    //onPress={() => navigation.navigate('Setting')}
                 />
             </View>
         </View>
@@ -114,13 +145,58 @@ const styles = StyleSheet.create({
         textAlign: 'center'
     },
 
-    nameOfInput: {
+    labelOfInput: {
         color: color.headerText,
         fontSize: 16,
         fontWeight: 'bold',
         alignSelf: 'flex-start',
         marginBottom: -10,
-    }
+    },
+
+    inputView: {
+        alignSelf: 'stretch',
+        height: 40,
+        marginBottom: 30,
+        borderBottomColor: color.border,
+        borderBottomWidth: 1,
+    },
+
+    inputText: {
+        height: 45,
+        color: color.inputText,
+    },
+
+    logoutButton: {
+        alignSelf: 'center',
+        backgroundColor: 'red',
+        borderRadius: 5,
+        height: 45,
+        width: 250,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 30,
+    },
+
+    logoutText: {
+        fontWeight: 'bold',
+        color: color.buttonText
+    },
+
+    changeInfoButton: {
+        marginVertical: 0,
+        alignSelf: 'stretch',
+        backgroundColor: color.button,
+        borderRadius: 5,
+        height: 45,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    changeInfoText: {
+        fontWeight: 'bold',
+        color: color.buttonText,
+        fontSize: 16,
+    },
 })
 
 
