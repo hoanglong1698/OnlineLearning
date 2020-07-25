2985de558b0ad59faf868c24da74f700f0e3bff4
Hoang Long <1612352@student.hcmus.edu.vn>
2020-07-25 17:22:13 +0700

    RefreshControl cho Home component

--- src/components/Main/Home/home.js
+++ src/components/Main/Home/home.js
@@ -1,15 +1,16 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { StyleSheet, ScrollView, Image, ActivityIndicator } from 'react-native';
+import { StyleSheet, ScrollView, Image, ActivityIndicator, RefreshControl } from 'react-native';
 import SectionCourses from './SectionCourses/section-courses';
 import { color } from './../../../globals/constants';
 import SectionAuthors from './SectionAuthors/section-authors';
-import SectionPaths from './SectionPaths/section-paths';
-import { continueLearning, bookmarks } from './../../../globals/database';
 import { ThemeContext } from '../../../provider/theme-provider';
 import { AuthenticationContext } from '../../../provider/authentication-provider';
 import axios from 'axios';
 
 const Home = (props) => {
+    const { theme } = useContext(ThemeContext)
+    const authContext = useContext(AuthenticationContext);
+
     const [state, setState] = useState({
         ContinueLearning: null,
         Favorites: null,
@@ -18,10 +19,9 @@ const Home = (props) => {
     });
 
     const [IsLoading, setIsLoading] = useState(true);
-    const { theme } = useContext(ThemeContext)
-    const authContext = useContext(AuthenticationContext);
+    const [refreshing, setRefreshing] = useState(false);
 
-    useEffect(() => {
+    const GetData = () => {
         axios.all([
             axios.get('https://api.itedu.me​/user/get-process-courses', {
                 headers: {
@@ -29,26 +29,43 @@ const Home = (props) => {
                     'Authorization': 'Bearer ' + authContext.state.token
                 },
             }),
+            axios.get('https://api.itedu.me/user/get-favorite-courses', {
+                headers: {
+                    'Authorization': 'Bearer ' + authContext.state.token
+                }
+            })
         ])
-            .then(axios.spread(function (resContinueLearning) {
+            .then(axios.spread(function (resContinueLearning, resFavorites) {
                 setState({
                     ContinueLearning: resContinueLearning.data.payload,
+                    Favorites: resFavorites.data.payload
                 });
-                setIsLoading(false);
             }))
-
             .catch(function (error) {
                 return (error);
+            })
+            .then(function () {
+                setIsLoading(false);
+                setRefreshing(false);
             });
 
-    }, [])
+    };
+
+    const onRefresh = () => {
+        setRefreshing(true);
+        GetData();
+    }
+
+    useEffect(GetData, []);
 
     return (
-        <ScrollView style={{ ...styles.container, backgroundColor: theme.mainBackgroundColor }}>
+        <ScrollView style={{ ...styles.container, backgroundColor: theme.mainBackgroundColor }}
+            refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+        >
             <Image style={styles.image} source={{ uri: 'https://cdn.pixabay.com/photo/2016/05/17/19/08/hyacinth-1398839_960_720.jpg' }}></Image>
             {IsLoading === true && <ActivityIndicator size="large" />}
-            <SectionCourses title='Khóa học đang học' navigation={props.navigation} data={state.ContinueLearning} />
-            <SectionCourses title='Khóa học yêu thích' navigation={props.navigation} data={bookmarks} />
+            {state.ContinueLearning != null && <SectionCourses title='Khóa học đang học' navigation={props.navigation} data={state.ContinueLearning} />}
+            {state.Favorites != null && <SectionCourses title='Khóa học yêu thích' navigation={props.navigation} data={state.Favorites} />}
             <SectionAuthors title='Author' />
         </ScrollView>
     )
