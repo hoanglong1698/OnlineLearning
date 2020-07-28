f836fda61b8877efb7049336a17ed30b40b74b95
Hoang Long <1612352@student.hcmus.edu.vn>
2020-07-28 21:20:23 +0700

    Cài đặt chức năng lấy các khóa học theo thể loại

--- src/components/Common/category-button.js
+++ src/components/Common/category-button.js
@@ -24,7 +24,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: 'rgba(0,0,0,0.5)'
+        backgroundColor: 'rgba(0,0,0,0.45)'
     },
 
     text: {
