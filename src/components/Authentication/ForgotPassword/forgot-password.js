12cd85404aa354594f9771df3679f489b11dddcb
Hoang Long <1612352@student.hcmus.edu.vn>
2020-08-11 21:27:02 +0700

    Đổi giao diện màn hình quên mật khẩu

--- src/components/Authentication/ForgotPassword/forgot-password.js
+++ src/components/Authentication/ForgotPassword/forgot-password.js
@@ -1,15 +1,48 @@
-import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { View, StyleSheet, TextInput, TouchableOpacity, Text, ActivityIndicator } from 'react-native'
 import { color } from '../../../globals/constants'
-import TouchableButton from '../../Common/touchable-button'
-import PasswordView from '../../Common/password-view'
+import axios from 'axios';
 
 const ForgotPassword = () => {
+    const [emailIsValid, setEmailIsValid] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const [email, setEmail] = useState('');
+
+    const onPressSubmit = () => {
+        if (emailIsValid) {
+            setIsLoading(true);
+        }
+    }
+
     return (
         <View style={styles.container}>
-            <PasswordView title="New password"></PasswordView>
-            <PasswordView title="Confirm new password"></PasswordView>
-            <TouchableButton title="Reset Password"></TouchableButton>
+            <View style={styles.inputView}>
+                <TextInput
+                    style={styles.inputText}
+                    placeholder='Email'
+                    placeholderTextColor={color.placeholderTextColor}
+                    keyboardType='email-address'
+                    autoCapitalize='none'
+                    onChangeText={text => {
+                        text = text.trim();
+                        let validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+                        if (validEmail.test(text)) {
+                            setEmailIsValid(true)
+                        }
+                        else {
+                            setEmailIsValid(false)
+                        }
+                        setEmail(text);
+                    }}
+                />
+            </View>
+            {!emailIsValid && <Text style={{ color: 'red' }}>Email không đúng định dạng</Text>}
+
+            {isLoading === true && <ActivityIndicator size="large" />}
+            <TouchableOpacity style={styles.button}>
+                <Text style={styles.signUpText}>Gửi mã xác nhận</Text>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -22,11 +55,33 @@ const styles = StyleSheet.create({
         paddingHorizontal: 50,
     },
 
-    text: {
-        alignSelf: 'flex-start',
-        color: color.headerText,
-        fontSize: 12,
-        marginBottom: 15,
+    inputView: {
+        alignSelf: 'stretch',
+        height: 40,
+        marginBottom: 30,
+        borderBottomColor: color.border,
+        borderBottomWidth: 1,
+    },
+
+    inputText: {
+        height: 45,
+        color: color.inputText,
+    },
+
+    button: {
+        marginVertical: 25,
+        alignSelf: 'stretch',
+        backgroundColor: color.button,
+        borderRadius: 5,
+        height: 45,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+
+    signUpText: {
+        fontWeight: 'bold',
+        color: color.buttonText,
+        fontSize: 16,
     },
 });
 
