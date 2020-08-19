a4cbd7efcd4ec5cbc9d286018ff850d9fa84a8d8
Hoang Long <1612352@student.hcmus.edu.vn>
2020-08-19 19:55:07 +0700

    Navigation vào màn hình kết quả tìm kiếm

--- src/components/Main/Search/result.js
+++ src/components/Main/Search/result.js
@@ -1,12 +1,64 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import SearchAll from './search-all';
+import SearchCourse from './search-course';
+import SearchInstructor from './search-instructor';
+import { color } from './../../../globals/constants';
+import { SearchBar } from 'react-native-elements';
+import { ThemeContext } from '../../../provider/theme-provider';
+import { AuthenticationContext } from '../../../provider/authentication-provider';
+import axios from 'axios';
 
-export default function Result() {
+const TopTab = createMaterialTopTabNavigator();
+export default function Result(props) {
+    console.log(props.route.params.string);
+    const [searchString, setSearchString] = useState(props.route.params.string);
+    const { theme } = useContext(ThemeContext)
+    const authContext = useContext(AuthenticationContext);
+
+    useEffect(() => {
+        axios.post('https://api.itedu.me/course/searchV2', {
+            token: authContext.state.token,
+            keyword: searchString,
+            limit: 20,
+            offset: 0
+        })
+            .then(function (response) {
+                console.log(response.data.payload);
+            })
+            .catch(function (error) {
+                console.log(error)
+            });
+    }, [])
+
     return (
-        <View>
-            <Text></Text>
+        <View style={styles.containter}>
+            <SearchBar
+                placeholder={searchString}
+                clearIcon
+                platform='default'
+                lightTheme={theme.lightSeachBar}
+            />
+
+            <TopTab.Navigator
+                independent={true}
+                initialRouteName="ALL"
+                tabBarOptions={{
+                    indicatorStyle: { height: 3, backgroundColor: color.headerBar },
+                    labelStyle: { fontWeight: 'bold' }
+                }}
+            >
+                <TopTab.Screen name="ALL" component={SearchAll} />
+                <TopTab.Screen name="COURSES" component={SearchCourse} />
+                <TopTab.Screen name="INSTRUCTORS" component={SearchInstructor} />
+            </TopTab.Navigator>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    containter: {
+        flex: 1,
+    }
+})
